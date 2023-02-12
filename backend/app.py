156f8bc68bf8e04be4f5ca15d528d57fef1360f2
Sagi Twig <twigsagi@gmail.com>
2023-02-12 23:28:01 +0200

    Add total count logic

--- backend/app.py
+++ backend/app.py
@@ -6,10 +6,21 @@ from flask_cors import CORS
 import json
 
 
+
 app = Flask(__name__)
 CORS(app)
 
 ITEMS_PER_PAGE = 10
+BASE_URL = "https://jira-soft.ngsoft.com/rest/api/2"
+NO_RESULT_SEARCH = "startAt=0&maxResults=0"
+
+
+def get_total_issues(jql, headers, BASE_URL, NO_RESULT_SEARCH):
+    response = requests.get(f'{BASE_URL}/search/?jql={jql}&{NO_RESULT_SEARCH}', headers=headers)
+    response_json = json.loads(response.text)
+    return response_json['total']
+
+
 
 @app.route('/jira/issue/<issue_id>', methods=['GET'])
 def jira_issue(issue_id):
@@ -28,7 +39,7 @@ def jira_issue(issue_id):
     }
 
     # Send the API request
-    response = requests.get(f'https://jira-soft.ngsoft.com/rest/api/2/issue/{issue_id}', headers=headers)
+    response = requests.get(f'{BASE_URL}/issue/{issue_id}', headers=headers)
 
     string_data = response.content.decode("utf-8")
 
@@ -42,8 +53,6 @@ def jira_jql(project_id):
     auth = base64.b64encode(b'sagi.twig:St123369').decode('utf-8')
     page = request.args.get('page', default=1, type=int)
 
-    limit = ITEMS_PER_PAGE
-
     # Set the Authorization header
     headers = {
         'Authorization': f'Basic {auth}',
@@ -56,20 +65,35 @@ def jira_jql(project_id):
 
 
     # Send the API request
-    response = requests.get(f'https://jira-soft.ngsoft.com/rest/api/2/search/?jql=project={project_id}&startAt={(page - 1) * limit}&maxResults={limit}', headers=headers)
-
+    response = requests.get(f'{BASE_URL}/search/?jql=project={project_id}&startAt={(page - 1) * ITEMS_PER_PAGE}&maxResults={ITEMS_PER_PAGE}', headers=headers)
     string_data = response.content.decode("utf-8")
 
+
+
+    total_issues = get_total_issues(f'project={project_id}', headers, BASE_URL, NO_RESULT_SEARCH)
+    total_issues_blocker = get_total_issues(f'project={project_id} AND severity="blocker"', headers, BASE_URL, NO_RESULT_SEARCH)
+    total_issues_critical = get_total_issues(f'project={project_id} AND severity="critical"', headers, BASE_URL, NO_RESULT_SEARCH)
+
+
+
+    res = {
+        "data": string_data,
+        "total_issues" : total_issues,
+        "total_blocker" : total_issues_blocker,
+        "total_critical" : total_issues_critical,
+    }
+
+
     # Return the response
-    print(project_id)
-    return make_response(string_data, 200)
+    print(total_issues_blocker)
+    return make_response(res, 200)
     
 
 @app.route('/jira/project', methods=['POST', 'GET'])
 def get_all_projects():
-    # Extract the page and limit parameters from the query string
+    # Extract the page parameters from the query string
     page = request.args.get('page', default=1, type=int)
-    limit = ITEMS_PER_PAGE
+
     # Base64 encode the credentials
     auth = base64.b64encode(b'sagi.twig:St123369').decode('utf-8')
 
@@ -85,7 +109,7 @@ def get_all_projects():
     }
 
     # Send the API request with the startAt and maxResults parameters
-    response = requests.get(f'https://jira-soft.ngsoft.com/rest/api/2/project?startAt={(page - 1) * limit}&maxResults={limit}', headers=headers)
+    response = requests.get(f'https://jira-soft.ngsoft.com/rest/api/2/project?startAt={(page - 1) * ITEMS_PER_PAGE}&maxResults={ITEMS_PER_PAGE}', headers=headers)
 
     string_data = response.content.decode("utf-8")
 
